b2267ba3ab8785224377a2074d7c5cc0b2380b29
phucdo4092004-lang <phucdo4092004@gmail.com>
2026-03-29 17:16:42 +0700

    Update KhoDuLieu.js

--- KhoDuLieu.js
+++ KhoDuLieu.js
@@ -9,6 +9,16 @@ Câu 1: Phúc đẹp trai ?
 * Quá đẹp trai
 • Siêu đẹp trai
     `
+  }, "khop_can_02": {
+    tenDe: "XEM BÓI FREE ",
+    moTa: "20 câu hỏi chuyên sâu để đoán trước tương lai",
+    soCau: 20,
+    noiDung:
+      Câu 1: Đi ngủ đi
+      * Chúc ngủ ngon 
+      * Không ngủ thì thôi`
+
+    `
   }
 };
 
