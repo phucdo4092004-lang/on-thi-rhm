2efbce5d72b591039ffa821084a612c8387df91d
phucdo4092004-lang <phucdo4092004@gmail.com>
2026-03-29 17:33:43 +0700

    Update KhoDuLieu.js

--- KhoDuLieu.js
+++ KhoDuLieu.js
@@ -1,20 +1,30 @@
 // === 1. KHU VỰC TRẮC NGHIỆM ===
 const KHO_DE_THI = {
   "khop_can_01": {
-    tenDe: "Phúc đẹp trai ",
-    moTa: "130 câu hỏi luyện tập để khen phúc đẹp trai",
+    tenDe: "Phúc có đẹp trai?",
+    moTa: "130 câu hỏi luyện tập khen sự đẹp trai của phúc",
     soCau: 130,
     noiDung: `
-Câu 1: Phúc đẹp trai ?
-* Quá đẹp trai
-• Siêu đẹp trai
+Câu 1: Phúc có đẹp trai không?
+* Phúc quá đẹp trai
+* Phúc quá đẹp trai
     `
+  },"khop_can_02": {
+    tenDe: "Bạn có hiểu bản thân bạn không?  ",
+    moTa: "Hoàn thành 20 câu hỏi chuyên sâu sau đây tôi sẽ trả lời cho bạn biết bạn là ai ",
+    soCau: 130,
+    noiDung: `
+Câu 1: Donate cho phúc chưa mà đòi :)?
+* 100k tạm
+• 1tr Ok
+    `
+  }
 };
 
 // === 2. KHU VỰC TÀI LIỆU ĐỌC ===
 const KHO_TAI_LIEU = {
   "ly_thuyet_01": {
-    tieuDe: "Phúc đẹp trai",
+    tieuDe: "Cẩm nang Khớp Cắn Học",
     moTa: "Tóm tắt lý thuyết trọng tâm",
     noiDung: `
 CHƯƠNG 1: CƠ SỞ KHỚP CẮN HỌC
@@ -25,10 +35,8 @@ Hệ thống sẽ giữ nguyên định dạng văn bản cho bạn.
     `
   },
   "meo_thi_01": {
-    tieuDe: "Phúc quá đẹp trai",
+    tieuDe: "Mẹo thi CCHN 2027",
     moTa: "Các lưu ý khi đi thi",
-    noiDung: `Câu 6: (1 đáp án) 
-Ghi vào đây
-`
+    noiDung: `Dán nội dung mẹo thi vào đây...`
   }
 };
